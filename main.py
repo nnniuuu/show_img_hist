f2d1770f0a724c5ad430885f60a81c2e28498c78
Junsei Kakefu <cmj14038@ict.nitech.ac.jp>
2022-07-26 20:16:01 +0900

    kadai

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/0/05/Deported_from_Crimea.jpg'
+img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
 
 im = imread(img_url)
 
-st.image(im, caption='image for kadai',
+st.image(im, caption='image from wikimedia commons',
          use_column_width=True)
 
 
